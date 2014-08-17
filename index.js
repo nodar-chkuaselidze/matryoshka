65969b262737ebac0adf6b8cf419fdb05d38efc3
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-17 15:03:21 +0400

    Add help

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-var argv = require('optimist').argv;
+var optimist = require('optimist'),
+    argv;
+
+argv = optimist
+  .usage('Pack the input, Usage: $0')
+  .alias('h', 'help')
+  .describe('h', 'Load this help')
+  .argv;
+
+if (argv.help) {
+  return optimist.showHelp();
+}
