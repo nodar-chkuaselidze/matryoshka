0b759c9da85b9638c7df0b1dc3e30875f6365a9a
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-17 15:17:27 +0400

    Rewrite to options

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 #!/usr/bin/env node
 
-var optimist = require('optimist'),
+var optimist   = require('optimist'),
     argv;
 
 argv = optimist
   .usage('Pack the input, Usage: $0')
-  .alias('h', 'help')
-  .describe('h', 'Load this help')
-  .alias('l', 'list')
-  .describe('l', 'List of languages')
-  .alias('i', 'include')
-  .describe('i', 'Include languages')
-  .alias('e', 'exclude')
-  .describe('e', 'Exclude languages')
-  .describe('x', 'Extract packed')
+  .options('h', {
+    alias : 'help',
+    describe : 'Load this help'
+  })
+  .options('l', {
+    alias : 'list',
+    describe : 'List available languages'
+  })
+  .options('x', {
+    alias : 'extract',
+    describe : 'Extract packed'
+  })
+  .options('i', {
+    alias : 'include',
+    describe : 'Include languages'
+  })
+  .options('e', {
+    alias : 'exclude',
+    describe : 'Exclude languages',
+  })
   .argv;
 
 if (argv.help) {
