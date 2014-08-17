cd42f833ec0e9e8ec9e228ed741e7f52955df128
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-17 15:10:13 +0400

    Add other cmd line options

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ argv = optimist
   .usage('Pack the input, Usage: $0')
   .alias('h', 'help')
   .describe('h', 'Load this help')
+  .alias('l', 'list')
+  .describe('l', 'List of languages')
+  .alias('i', 'include')
+  .describe('i', 'Include languages')
+  .alias('e', 'exclude')
+  .describe('e', 'Exclude languages')
+  .describe('x', 'Extract packed')
   .argv;
 
 if (argv.help) {
