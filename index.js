a18c0246b9f7993fcf242657e124f154bee2c05e
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-17 19:21:01 +0400

    Implement List

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 var optimist   = require('optimist'),
-    argv;
+    Matryoshka = require('./lib/matryoshka'),
+    Extract    = require('./lib/extract'),
+    matryoshka, argv;
 
 argv = optimist
   .usage('Pack the input, Usage: $0')
@@ -19,14 +21,34 @@ argv = optimist
   })
   .options('i', {
     alias : 'include',
-    describe : 'Include languages'
+    describe : 'Include languages, comma separated "js,c"',
   })
   .options('e', {
     alias : 'exclude',
-    describe : 'Exclude languages',
+    describe : 'Exclude languages, comma separated "js,c"',
   })
   .argv;
 
 if (argv.help) {
   return optimist.showHelp();
 }
+
+if (argv.list) {
+  console.log('  List of languages:');
+
+  Matryoshka.collectLanguages().then(function (languages) {
+    Object.keys(languages).forEach(function (language) {
+      console.log('   ', language);
+    });
+  });
+
+  return;
+}
+
+if (argv.x) {
+  matryoshka = new Extract();
+  matryoshka.extract();
+  return;
+}
+
+matryoshka = new Matryoshka();
