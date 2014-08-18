ca663c238dace81f0e05011aa503345ce9ff3403
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-18 13:07:35 +0400

    Move CLI class to seperate file

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-var optimist   = require('optimist'),
-    path       = require('path'),
-    fs         = require('fs'),
-    Matryoshka = require('./lib/matryoshka'),
-    Extract    = require('./lib/extract'),
-    matryoshka, argv,
-    stdin, stdout;
+var optimist = require('optimist'), argv;
 
 argv = optimist
   .usage('Pack the input, Usage: $0')
@@ -45,63 +39,4 @@ argv = optimist
   })
   .argv;
 
-function CLI(argv) {
-  this.argv = argv;
-  this.in   = process.stdin;
-  this.out  = process.stdout;
-
-  if (argv.help)   return this.help();
-  if (argv.list)   return this.list();
-  if (argv.file)   this.fileInit();
-  if (argv.output) this.outputInit();
-};
-
-CLI.prototype.fileInit = function () {
-  this.file = path.resolve(this.argv.file);
-
-  if (!fs.existsSync(this.file)) {
-    this.error('File "' + this.file + '" not found');
-  }
-
-  var stats = fs.statSync(this.file);
-  if (stats.isFile()) {
-    this.in = fs.createReadStream(this.file);
-  }
-};
-
-CLI.prototype.outputInit = function () {
-  var self = this;
-
-  this.output = path.resolve(this.argv.output);
-  if (fs.existsSync(this.output)) {
-    this.error('File "' + this.output + '" already exists, won\'t overwrite');
-  }
-
-  this.out = fs.createWriteStream(this.output);
-  this.out.on('error', function () {
-    self.error('Could not write to file "' + self.output + '"');
-  });
-};
-
-CLI.prototype.help = function () {
-  optimist.showHelp();
-};
-
-CLI.prototype.list = function () {
-  console.log('  List of languages:');
-
-  Matryoshka.collectLanguages().then(function (languages) {
-    Object.keys(languages).forEach(function (language) {
-      console.log('   ', language);
-    });
-  });
-};
-
-CLI.prototype.error = function (error, errcode) {
-  errcode = errcode || 1;
-
-  console.error(error);
-  process.exit(errcode);
-}
-
-new CLI(argv);
+new require('./lib/CLI')(argv);
