1a1283ebc5e644f28f3c415db679182196efa11c
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-23 14:27:12 +0400

    Add collectLanguages test case

--- test/matryoshka.js
+++ test/matryoshka.js
@@ -1,11 +1,22 @@
 var should = require('should'),
-  Matryoshka = require('../lib/').Matryoshka;
+  Matryoshka = require('../lib/').Matryoshka,
+  langs;
 
 describe('Matryoshka Packer API', function () {
   it('should have static method languagePackers to collect packers based on language', function (done) {
     Matryoshka.languagePackers('js').then(function (cPackers) {
       cPackers.should.be.an.Array;
       done();
+    });
+  });
+
+  it('should have static method collectLanguages to collect all languages', function (done) {
+    Matryoshka.collectLanguages().then(function (languages) {
+      languages.should.be.an.instanceOf(Object);
+
+      Object.keys(languages).forEach(function (lang) {
+        lang.should.be.an.instanceOf(Array);
+      });
     });
   });
 });
