e301f539a42e62043bc90e823569ce8188b3d8c9
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-08-23 20:15:00 +0400

    should take random language out of Languages Object

--- test/matryoshka.js
+++ test/matryoshka.js
@@ -1,8 +1,11 @@
 var should = require('should'),
   Matryoshka = require('../lib/').Matryoshka,
+  Qfs = require('q-io/fs'),
   langs;
 
 describe('Matryoshka Packer API', function () {
+  var matryoshka = new Matryoshka(0, process.stdin, process.stdout);
+
   it('should have static method languagePackers to collect packers based on language', function (done) {
     Matryoshka.languagePackers('js').then(function (cPackers) {
       cPackers.should.be.an.instanceOf(Array);
@@ -28,9 +31,19 @@ describe('Matryoshka Packer API', function () {
     });
   });
 
-  it('should include languages that are in include list', function (done) {
-    var matryoshka = new Matryoshka(0, process.stdin, process.stdout);
+  it('should take random language from langs object', function (done) {
+    Matryoshka.collectLanguages().then(function (languages) {
+      var randPacker = Matryoshka.getRandomLanguage(languages);
 
+      randPacker.should.be.type('string');
+      randPacker.should.endWith('.js');
+    })
+    .fail(function (error) {
+      done(error);
+    });
+  });
+
+  it('should include languages that are in include list', function (done) {
     matryoshka.processLanguages('c,js', true)
       .then(function (languages) {
         var keys = Object.keys(languages);
@@ -47,8 +60,6 @@ describe('Matryoshka Packer API', function () {
   });
 
   it('should exclude languages that are in exclude list', function (done) {
-    var matryoshka = new Matryoshka(0, process.stdin, process.stdout);
-
     matryoshka.processLanguages('c,js', false)
       .then(function (languages) {
         languages.should.not.have.property('c');
